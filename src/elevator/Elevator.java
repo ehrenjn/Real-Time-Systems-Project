60d94d4af6470b2e6140d0a5e79bd9b4927ebcee
Christopher Wang <christopher.jj.wang@gmail.com>
2020-01-29 12:20:35 -0500

    Added Functionality to the scheduler

--- src/elevator/Elevator.java
+++ src/elevator/Elevator.java
@@ -1,8 +1,12 @@
 package elevator;
 
+import common.Direction;
+
 public class Elevator{
 	private ElevatorButton[] elevatorButtons;
 	private ElevatorLamp[] evelatorLamps;
 	private Motor motor;
 	private Door door;
+	private Direction direction;
+	private int floor;
 }
--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -1,6 +1,30 @@
 package scheduler;
 
+import common.CommunicationSocket;
+import common.Event;
 
 public class Scheduler {
-
+	private CommunicationSocket elevatorSocket;
+	private CommunicationSocket floorSocket;
+	
+	public Scheduler(CommunicationSocket elevatorSocket, CommunicationSocket floorSocket) {
+		this.elevatorSocket = elevatorSocket;
+		this.floorSocket = floorSocket;
+	}
+	
+	public void sendFloorEventIn(Event event) {
+		this.floorSocket.sendEventIn(event);
+	}
+	
+	public Event recieveFloorEventOut() {
+		return this.floorSocket.recieveEventOut();
+	}
+	
+	public void sendElevatorEventIn(Event event) {
+		this.elevatorSocket.sendEventIn(event);
+	}
+	
+	public Event recieveElevatorEventOut() {
+		return this.elevatorSocket.recieveEventOut();
+	}
 }
