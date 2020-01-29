31482dc3ca7e982424f6f72e5bd14058a93b3571
ehrenjn <ehrenjulienneitzert@cmail.carleton.ca>
2020-01-29 13:20:26 -0500

    moved message passing to Elevator

--- src/elevator/Elevator.java
+++ src/elevator/Elevator.java
@@ -1,12 +1,34 @@
 package elevator;
 
+import common.CommunicationSocket;
 import common.Direction;
+import common.Event;
 
-public class Elevator{
+public class Elevator {
 	private ElevatorButton[] elevatorButtons;
 	private ElevatorLamp[] evelatorLamps;
 	private Motor motor;
 	private Door door;
 	private Direction direction;
 	private int floor;
+	private CommunicationSocket elevatorSocket;
+	
+	/**
+	 * Creates a new Elevator
+	 * @param elevatorSocket the socket for the elevator to receive and send events on
+	 */
+	public Elevator(CommunicationSocket elevatorSocket) {
+		this.elevatorSocket = elevatorSocket;
+	}
+	
+	/**
+	 * Receives an event, does work, then sends an event back to the server
+	 */
+	public void doWork() {
+		Event event = this.elevatorSocket.recieveEventIn();
+		System.out.println("Elevator recieved elevator event in: " + event);
+		//Do work on message
+		this.elevatorSocket.sendEventOut(event);
+		System.out.println("Elevator sent elevator event out: " + event);
+	}
 }
--- src/elevator/ElevatorSubsystem.java
+++ src/elevator/ElevatorSubsystem.java
@@ -6,22 +6,20 @@ import common.CommunicationSocket;
 import common.Event;
 import floor.EventReader;
 
-public class ElevatorSubsystem implements Runnable{
+public class ElevatorSubsystem implements Runnable {
 	private Elevator[] elevators;
-	private CommunicationSocket elevatorSocket;
+	private static int NUM_ELEVATORS = 1;
 	
 	public ElevatorSubsystem(CommunicationSocket elevatorSocket) {
-		this.elevatorSocket = elevatorSocket;
+		elevators = new Elevator[] {new Elevator(elevatorSocket)};
 	}
 	
 	
 	public void run() {
 		while(true) {
-			Event event = this.elevatorSocket.recieveEventIn();
-			System.out.println("Elevator recieved elevator event in: " + event);
-			//Do work on message
-			this.elevatorSocket.sendEventOut(event);
-			System.out.println("Elevator sent elevator event out: " + event);
+			for (Elevator elevator: elevators) {
+				elevator.doWork();
+			}
 		}
 	}
 	
