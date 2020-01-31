35b8850c8316433f0e7b626094fe775efdba2a13
Nathan Fohkens <nfohkens@gmail.com>
2020-01-30 19:27:55 -0500

    Adding details to Button and Lamp
    Suggestion to merge Buttom and Lamp into a single class.

--- src/common/Button.java
+++ src/common/Button.java
@@ -1,5 +1,27 @@
 package common;
 
+/**
+ * Button
+ * 
+ * A Button has the following properties:
+ * - Location (Elevator #, Floor #)
+ * - Type (defines the type of event it causes)
+ * - Lamp (each button is associated with a Lamp, each Lamp is associated with a Button)
+ * 
+ * When pressed, a button causes a(n):
+ * - Event to occur
+ * - Lamp to turn on
+ */
 public class Button {
+    private Lamp lamp;
 
+    /**
+     * @return the status of the Button's lamp. TRUE = ON; FALSE = OFF
+     */
+    public boolean getStatus() {
+        return lamp.getStatus();
+    }
+
+    // When button is pressed, call the following method:
+    // lamp.setLampOn();
 }
--- src/common/Lamp.java
+++ src/common/Lamp.java
@@ -1,5 +1,41 @@
 package common;
 
+/**
+ * Lamp
+ * 
+ * A Lamp has the following properties:
+ * - Status
+ * - Associated Button
+ * 
+ * A Lamp can be turned on  only when the associated Button is pressed.
+ *      This is accomplished by having the method public, but only available to call by it's associated Button.
+ * 
+ * A Lamp can be turned off only when the associated Floor is reached by the Elevator.
+ *      This is still TO-DO.
+ */
 public class Lamp {
+    private boolean lampStatus;
 
+    /** 
+     * @return returns the boolean status of the Lamp. TRUE = ON; FALSE = OFF
+     */
+    public boolean getStatus() {
+        return lampStatus;
+    }
+
+    /**
+     * @return set the Lamp status to ON (TRUE)
+     */
+    public void setLampOn() {
+        lampStatus = true;
+        return;
+    }
+
+    /**
+     * @return set the Lamp status to OFF (FALSE)
+     */
+    public void setLampOff() {
+        lampStatus = false;
+        return;
+    }
 }
