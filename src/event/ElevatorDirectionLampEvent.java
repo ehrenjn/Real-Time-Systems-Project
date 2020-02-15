3212800f32a5e6c75fa76a853a162858d1c0d339
Christopher Wang <christopher.jj.wang@gmail.com>
2020-02-15 12:43:05 -0500

    Changed ElevatorDirectionLampEvent to now break up the elevator lamp state

--- src/event/ElevatorDirectionLampEvent.java
+++ src/event/ElevatorDirectionLampEvent.java
@@ -7,20 +7,35 @@ import common.*;
  *
  */
 public class ElevatorDirectionLampEvent extends Event {
-	private DirectionLampState lampState;
+	private LampState lampState;
+	private Direction lampDirection;
 	public static String NAME = "ElevatorDirectionLampEvent";
 	
 	/**
 	 * Creates a new ElevatorFloorLampEvent
 	 * @param lampState the state that the direction lamp should be in
+	 * @param lampDirection the direction of the lamp to turn on or off
 	 */
-	public ElevatorDirectionLampEvent(DirectionLampState lampState) {
+	public ElevatorDirectionLampEvent(LampState lampState, Direction lampDirection) {
 		super(NAME);
 		this.lampState = lampState;
+		this.lampDirection = lampDirection;
 	}
 	
-	public DirectionLampState getLampState() {
+	public LampState getLampState() {
 		return lampState;
+	}
+
+	public void setLampState(LampState lampState) {
+		this.lampState = lampState;
+	}
+
+	public Direction getLampDirection() {
+		return lampDirection;
+	}
+
+	public void setLampDirection(Direction lampDirection) {
+		this.lampDirection = lampDirection;
 	}
 }
 
