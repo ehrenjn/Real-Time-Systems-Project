0213e8959d7c38036aaae6ea628535e707fead2d
ehrenjn <ehrenjulienneitzert@cmail.carleton.ca>
2020-03-06 20:33:01 -0500

    added javadoc to EventQueue

--- src/scheduler/EventQueue.java
+++ src/scheduler/EventQueue.java
@@ -4,23 +4,38 @@ import java.util.LinkedList;
 
 import event.Event;
 
+/**
+ * A thread safe queue of events
+ *
+ */
+
 public class EventQueue {
 	
 	private LinkedList<Event> queue;
 	
-	
+	/**
+	 * Creates a new event queue
+	 */
 	public EventQueue() {
 		queue = new LinkedList<Event>();
 	}
 	
 	
-	public void addEvent(Event event) {
+	/**
+	 * Adds a new event to the queue
+	 * @param event the event to add
+	 */
+	public synchronized void addEvent(Event event) {
 		queue.add(event);
 		notifyAll();
 	}
 	
 	
-	public Event getNextEvent() {
+	/**
+	 * Blocks until there is an event available 
+	 * @return the event received
+	 */
+	public synchronized Event getNextEvent() {
 		while (queue.isEmpty()) {
 			try {
 				wait();
