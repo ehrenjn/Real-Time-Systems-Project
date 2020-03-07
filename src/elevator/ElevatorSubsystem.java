7e7511411f17474be6eb5ee055cd3653d20135c3
naomiluihing <naomiluihing@cmail.carleton.ca>
2020-03-07 14:53:29 -0500

    Input IP functionality in NetworkHelpers.java

--- src/elevator/ElevatorSubsystem.java
+++ src/elevator/ElevatorSubsystem.java
@@ -3,17 +3,22 @@ package elevator;
 import java.lang.management.ThreadInfo;
 import java.util.ArrayList;
 import java.util.List;
+import java.net.InetAddress;
 
 import common.CommunicationSocket;
 import common.Constants;
 import event.Event;
 import floor.EventReader;
+import network.NetworkHelpers;
 
 public class ElevatorSubsystem {	
 	public static void main(String args[]) {
 		Elevator[] elevators = new Elevator[Constants.NUM_ELEVATORS];
+		
+		InetAddress ip = NetworkHelpers.getIPFromInput();
+		
 		for (int id = 0; id < Constants.NUM_ELEVATORS; id++) {
-			Elevator elevator = new Elevator(Constants.NUM_FLOORS, id);
+			Elevator elevator = new Elevator(Constants.NUM_FLOORS, id,ip);
 			new Thread(elevator).start();
 		}
 	}
